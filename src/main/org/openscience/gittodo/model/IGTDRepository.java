8fa86c48deaae3287a439cbbbea5fee5e0f7290f
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 10:26:36 +0200

    Extracted an interface, and added a default implementation to search for HOME/.gtdrc

---
+++ src/main/org/openscience/gittodo/model/IGTDRepository.java
@@ -0,0 +1,8 @@
+package org.openscience.gittodo.model;
+
+public interface IGTDRepository {
+
+	public String getLocation();
+	public void setLocation(String location);
+
+}
--- src/main/org/openscience/gittodo/model/Repository.java
+++ src/main/org/openscience/gittodo/model/Repository.java
@@ -5,16 +5,43 @@
  */
 package org.openscience.gittodo.model;
 
-public class Repository {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
-	private static String location;
+public class Repository implements IGTDRepository {
 
-	public static String getLocation() {
-		return Repository.location;
+	private String location;
+
+	public Repository() {
+		Properties reposProps = new Properties();
+		try {
+			reposProps.load(
+				new FileReader(
+					new File(
+						System.getProperty("user.home") + File.separator + 
+						".gtdrc"
+					)
+				)
+			);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		location = reposProps.getProperty("Repository");
+	}
+	
+	public String getLocation() {
+		return this.location;
 	}
 
-	public static void setLocation(String location) {
-		Repository.location = location;
+	public void setLocation(String location) {
+		this.location = location;
 	}
 
 }
