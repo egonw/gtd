f8ea557ab0d69f7f98116238e02e5f2887ef0769
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 21:40:55 +0200

    Added gtd-set-priority

---
+++ src/main/org/openscience/gittodo/app/SetPriority.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * License: LGPL v3
+ */
+package org.openscience.gittodo.app;
+
+import java.util.Map;
+
+import org.openscience.gittodo.io.ItemWriter;
+import org.openscience.gittodo.model.IGTDRepository;
+import org.openscience.gittodo.model.Item;
+import org.openscience.gittodo.model.Repository;
+
+public class SetPriority {
+	
+	public static void main(String[] args) throws Exception {
+		if (args.length < 2) {
+			System.out.println("Syntax: set-priority <PRIORITY> <ITEM-ID> <ITEM-ID-2> ...");
+			System.exit(0);
+		}
+		IGTDRepository repos = new Repository();
+		Map<Integer,Item> items = repos.items();
+		String priorityString = args[0];
+		Item.PRIORITY priority = Item.PRIORITY.NOW;
+		if ("NOW".equals(priorityString)) {
+			priority = Item.PRIORITY.NOW;
+		} else if ("DELAYED".equals(priorityString)) {
+			priority = Item.PRIORITY.DELAYED;
+		} else if ("HIGH".equals(priorityString)) {
+			priority = Item.PRIORITY.HIGH;
+		} else if ("MEDIUM".equals(priorityString)) {
+			priority = Item.PRIORITY.MEDIUM;
+		} else if ("LOW".equals(priorityString)) {
+			priority = Item.PRIORITY.LOW;
+		} else {
+			System.out.println("Unknown priority: " + priorityString);
+			System.exit(-1);
+		}
+		for (int i=1; i< args.length; i++) {
+			Integer itemID = Integer.parseInt(args[i]);
+			Item item = items.get(itemID);
+			if (item == null) {
+				System.out.println("No item with ID: " + itemID);
+			} else {
+				item.setPriority(priority);
+				ItemWriter writer = new ItemWriter(item);
+				writer.write();
+				writer.close();
+			}
+		}
+	}
+	
+}
