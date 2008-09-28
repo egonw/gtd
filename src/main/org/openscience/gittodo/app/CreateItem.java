dc65702ec32f9d7b8e4d4779b4955da7c808f3c2
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 10:25:28 +0200

    Implemented creating a item. Well, not really...

--- src/main/org/openscience/gittodo/app/CreateItem.java
+++ src/main/org/openscience/gittodo/app/CreateItem.java
@@ -8,7 +8,17 @@ package org.openscience.gittodo.app;
 public class CreateItem {
 	
 	public static void main(String[] args) {
-		
+		if (args.length < 2) {
+			System.out.println("Syntax: create-item <DATE> <TEXT>");
+		}
+		String date = args[0];
+		StringBuffer text = new StringBuffer();
+		for (int i=1; i<args.length; i++) {
+			text.append(args[i]);
+			if ((i+1)<args.length) text.append(' ');
+		}
+		System.out.println("Date: " + date);
+		System.out.println("Text: " + text);
 	}
 	
 }
