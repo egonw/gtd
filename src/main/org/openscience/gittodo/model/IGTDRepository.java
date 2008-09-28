c3c3bc56dcb72c33ff85354e658f384cdd60ee84
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 10:31:49 +0200

    Added missing license and method to get all items

--- src/main/org/openscience/gittodo/model/IGTDRepository.java
+++ src/main/org/openscience/gittodo/model/IGTDRepository.java
@@ -1,8 +1,16 @@
+/*
+ * Copyright 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * License: LGPL v3
+ */
 package org.openscience.gittodo.model;
+
+import java.util.List;
 
 public interface IGTDRepository {
 
 	public String getLocation();
 	public void setLocation(String location);
+	public List<Item> items();
 
 }
