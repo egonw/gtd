4c4adc2d5285b55008255c45e6502f41e14f35dc
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 12:28:48 +0200

    Added state support

---
+++ src/main/org/openscience/gittodo/app/SetDone.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * License: LGPL v3
+ */
+package org.openscience.gittodo.app;
+
+import java.util.Map;
+
+import org.openscience.gittodo.io.ItemWriter;
+import org.openscience.gittodo.model.IGTDRepository;
+import org.openscience.gittodo.model.Item;
+import org.openscience.gittodo.model.Repository;
+
+public class SetDone {
+	
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.out.println("Syntax: set-done <ITEM-ID>");
+		}
+		Integer itemID = Integer.parseInt(args[0]);
+		IGTDRepository repos = new Repository();
+		Map<Integer,Item> items = repos.items();
+		Item item = items.get(itemID);
+		if (item == null) {
+			System.out.println("No item with ID: " + itemID);
+		} else {
+			item.setState(Item.STATE.CLOSED);
+			ItemWriter writer = new ItemWriter(item);
+			writer.write();
+			writer.close();
+		}
+	}
+	
+}
