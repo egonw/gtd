0ed4cce5ef0bef971eaf1951195e8961180c46ad
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 10:04:26 +0200

    Added first cmd wrapper app dummy

---
+++ src/main/org/openscience/gittodo/app/CreateItem.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * License: LGPL v3
+ */
+package org.openscience.gittodo.app;
+
+public class CreateItem {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
