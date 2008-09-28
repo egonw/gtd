5f3a4214dc61eee7d3bb3f1859aaef6792aeb729
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 10:58:59 +0200

    Implemented ListItems cmd util

--- src/main/org/openscience/gittodo/app/ListItems.java
+++ src/main/org/openscience/gittodo/app/ListItems.java
@@ -6,6 +6,7 @@
 package org.openscience.gittodo.app;
 
 import org.openscience.gittodo.model.IGTDRepository;
+import org.openscience.gittodo.model.Item;
 import org.openscience.gittodo.model.Repository;
 
 public class ListItems {
@@ -13,6 +14,11 @@ public class ListItems {
 	public static void main(String[] args) {
 		IGTDRepository repos = new Repository();
 		System.out.println("Repository: " + repos.getLocation());
+		for (Item item : repos.items()) {
+			System.out.print(item.getCreationDate());
+			System.out.println(" ");
+			System.out.print(item.getText());
+		}
 	}
 	
 }
--- src/main/org/openscience/gittodo/model/Repository.java
+++ src/main/org/openscience/gittodo/model/Repository.java
@@ -9,7 +9,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+
+import org.openscience.gittodo.io.ItemReader;
 
 public class Repository implements IGTDRepository {
 
@@ -42,6 +46,38 @@ public class Repository implements IGTDRepository {
 
 	public void setLocation(String location) {
 		this.location = location;
+	}
+
+	public List<Item> items() {
+		List<Item> items = loadFromDir(new File(getLocation()));
+		return items;
+	}
+
+	private List<Item> loadFromDir(File dir) {
+		List<Item> items = new ArrayList<Item>();
+		File[] files = dir.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				items.addAll(loadFromDir(file));
+			} else if (file.getName().endsWith(".gtd")) {
+				// add file
+				ItemReader reader;
+				try {
+					reader = new ItemReader(
+						new FileReader(file)
+					);
+					Item item = reader.read();
+					items.add(item);
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return items;
 	}
 
 }
