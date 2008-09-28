850d53b64dc53937ab4a1ce3b9ec2041d4039516
Egon Willighagen <egonw@egonw-laptop.(none)>
2008-09-28 10:58:08 +0200

    Implemented .props file based item reading/writing

---
+++ src/main/org/openscience/gittodo/io/ItemReader.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2008  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * License: LGPL v3
+ */
+package org.openscience.gittodo.io;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
+import org.openscience.gittodo.model.Item;
+
+public class ItemReader {
+
+	private Reader reader;
+
+	public ItemReader(Reader reader) throws IOException {
+		this.reader = reader;
+	}
+
+	public Item read() throws Exception {
+		Properties itemProps = new Properties();
+		itemProps.load(reader);
+		Item item =  new Item(
+			itemProps.getProperty("CreationDate"),
+			itemProps.getProperty("Text")
+		);
+		return item;
+	}
+	
+	public void close() throws IOException {
+		reader.close();
+	}
+	
+}
--- src/main/org/openscience/gittodo/io/ItemWriter.java
+++ src/main/org/openscience/gittodo/io/ItemWriter.java
@@ -8,44 +8,37 @@ package org.openscience.gittodo.io;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Properties;
 
 import org.openscience.gittodo.model.Item;
 import org.openscience.gittodo.model.Repository;
 
 public class ItemWriter {
 
-	private PrintWriter writer;
+	private Writer writer;
 	private Item item;
 
 	public ItemWriter(Item item) throws IOException {
 		this.item = item;
 		File itemFile = createFile(item);
-		this.writer = new PrintWriter(
-			new FileWriter(itemFile)
-	    );
+		this.writer = new FileWriter(itemFile);
 	}
 
 	public void close() throws IOException {
 		writer.close();
 	}
 	
-	public void write() {
-		writer.println("[Item-v1]");
-		writeItemField("CreationDate", item.getCreationDate());
-		writeItemField("State", item.getState());
-		
-	}
-
-	private void writeItemField(String field, Object value) {
-		writer.println(field + "<<");
-		writer.println("" + value);
-		writer.println(">>");
+	public void write() throws IOException {
+		Properties itemProps = new Properties();
+		itemProps.put("CreationDate", item.getCreationDate());
+		itemProps.put("Text", item.getText());
+		itemProps.store(writer, "huh?");
 	}
 
 	public File createFile(Item item) {
 		return new File(
-			Repository.getLocation() + File.separator + 
+			new Repository().getLocation() + File.separator + 
 			item.getCreationDate() + File.separator +
 			item.hashCode() + ".gtd"
 		); 
