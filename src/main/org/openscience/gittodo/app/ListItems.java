43707f4480e3a270419155b860e98147a33c905b
Egon Willighagen <egon.willighagen@gmail.com>
2008-10-03 08:53:09 +0200

    ListItems now uses the Apache CLI library

--- src/main/org/openscience/gittodo/app/ListItems.java
+++ src/main/org/openscience/gittodo/app/ListItems.java
@@ -8,6 +8,12 @@ package org.openscience.gittodo.app;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
 import org.openscience.gittodo.format.OneLiner;
 import org.openscience.gittodo.model.IGTDRepository;
 import org.openscience.gittodo.model.Item;
@@ -16,15 +22,27 @@ import org.openscience.gittodo.sort.ItemSorter;
 
 public class ListItems {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+        // create Options object
+        Options options = new Options();
+        options.addOption(new Option("h", "home", false, "only items with the context HOME"));
+        options.addOption(new Option("w", "work", false, "only items with the context WORK"));
+        CommandLineParser parser = new PosixParser();
+        CommandLine cmd = null;
+        try {
+        	cmd = parser.parse(options, args);
+        } catch (Exception e) {
+        	HelpFormatter formatter = new HelpFormatter();
+        	formatter.printHelp( "gtd-list-items", options );
+        	System.exit(-1);
+		}
+
 		IGTDRepository repos = new Repository();
 		Item.CONTEXT context = null;
-		if (args.length > 0) {
-			if ("HOME".equals(args[0])) {
-				context = Item.CONTEXT.HOME;
-			} else if ("WORK".equals(args[0])) {
-				context = Item.CONTEXT.WORK;
-			}
+		if (cmd.hasOption("h")) {
+			context = Item.CONTEXT.HOME;
+		} else if (cmd.hasOption("w")) {
+			context = Item.CONTEXT.WORK;
 		}
 		List<Item> items = new ArrayList<Item>();
 		items.addAll(repos.items().values());
