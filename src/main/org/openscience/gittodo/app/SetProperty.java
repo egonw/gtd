ff561bd914fb4f12e65ef9600445f5d5a5ba75d6
Egon Willighagen <egon.willighagen@gmail.com>
2008-10-05 12:55:48 +0200

    Added gtd-set-property --priorty= --project=

--- src/main/org/openscience/gittodo/app/SetProperty.java
+++ src/main/org/openscience/gittodo/app/SetProperty.java
@@ -17,6 +17,7 @@ import org.apache.commons.cli.PosixParser;
 import org.openscience.gittodo.io.ItemWriter;
 import org.openscience.gittodo.model.IGTDRepository;
 import org.openscience.gittodo.model.Item;
+import org.openscience.gittodo.model.ItemTool;
 import org.openscience.gittodo.model.Repository;
 
 public class SetProperty {
@@ -25,7 +26,8 @@ public class SetProperty {
         // create Options object
         Options options = new Options();
         options.addOption(new Option("c", "context", true, "new context"));
-        // options.addOption(new Option("p", "priority", true, "new priority"));
+        options.addOption(new Option("p", "priority", true, "new priority"));
+        options.addOption(new Option("r", "project", true, "new project"));
         CommandLineParser parser = new PosixParser();
         CommandLine cmd = null;
         try {
@@ -45,16 +47,19 @@ public class SetProperty {
         }
 
 		Item.CONTEXT context = null;
-		if (cmd.hasOption("c")) {
-			context = cmd.getOptionValue("c").equals("HOME") ? Item.CONTEXT.HOME : Item.CONTEXT.WORK;
-			System.out.println("New context: " + context);
-		}
+		Item.PRIORITY priority = null;
+		String project = null;
+		if (cmd.hasOption("c")) context = ItemTool.parseContext(cmd.getOptionValue("c"));
+		if (cmd.hasOption("p")) priority = ItemTool.parseProperty(cmd.getOptionValue("p"));
+		if (cmd.hasOption("r")) project = cmd.getOptionValue("r");
 
 		IGTDRepository repos = new Repository();
 		for (String itemIDStr : itemIDs) {
 			Integer itemID = Integer.parseInt(itemIDStr);
 			Item item = repos.items().get(itemID);
 			if (context != null) item.setContext(context);
+			if (priority != null) item.setPriority(priority);
+			if (project != null) item.setProject(project);
 			
 			if (item.isChanged()) {
 				ItemWriter writer = new ItemWriter(item);
