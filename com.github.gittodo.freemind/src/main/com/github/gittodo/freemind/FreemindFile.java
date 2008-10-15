264af3867c677121ec68bcc27ad4094c9c24feac
Egon Willighagen <egon.willighagen@gmail.com>
2008-10-15 22:13:14 +0200

    Add icons for: 1. 'attach' for projects with open items; 2. 'warning' for TODAY and URGENT

--- com.github.gittodo.freemind/src/main/com/github/gittodo/freemind/FreemindFile.java
+++ com.github.gittodo.freemind/src/main/com/github/gittodo/freemind/FreemindFile.java
@@ -2,7 +2,7 @@ package com.github.gittodo.freemind;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,17 +13,19 @@ import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.Elements;
+import nu.xom.Serializer;
 
 import org.openscience.gittodo.model.IProject;
+import org.openscience.gittodo.model.Item.PRIORITY;
 
 public class FreemindFile {
 
     private FreemindProject root;
-    private Map<FreemindProject,FreemindProject> parentProjects;
+    private Map<String,FreemindProject> parentProjects;
     private Map<String,FreemindProject> projectNames;
     
     public FreemindFile(File file) throws Exception {
-        parentProjects = new HashMap<FreemindProject,FreemindProject>();
+        parentProjects = new HashMap<String,FreemindProject>();
         projectNames = new HashMap<String,FreemindProject>();
         if (file.exists()) {
             FileInputStream stream = new FileInputStream(file);
@@ -34,7 +36,7 @@ public class FreemindFile {
             stream.close();
         } else {
             root = new FreemindProject();
-            root.setName("All");
+            root.setName("  All  ");
         }
     }
 
@@ -62,13 +64,14 @@ public class FreemindFile {
             childProject.setLeftSibling("left".equals(child.getAttributeValue("POSITION")));
             childProject.setRightSibling("right".equals(child.getAttributeValue("POSITION")));
             childProject.setFolded("true".equals(child.getAttributeValue("FOLDED")));
-            parentProjects.put(childProject, parentProject);
+            parentProjects.put(childProject.getName(), parentProject);
             projectNames.put(childProject.getName(), childProject);
             processChildProjects(child, childProject);
         }
     }
 
-    private void createChildProjects(Element parent, FreemindProject parentProject) {
+    private PRIORITY createChildProjects(Element parent, FreemindProject parentProject) {
+        PRIORITY maxPriority = parentProject.getMaxPriority();
         for (FreemindProject child : getChildren(parentProject)) {
             if (child.getName() != null) {
                 Element childElem = new Element("node");
@@ -81,10 +84,37 @@ public class FreemindFile {
                 if (child.isFolded()) {
                     childElem.addAttribute(new Attribute("FOLDED","true"));
                 }
+//                STUFF to add info on what's open for a project
+                if (child.getOpenCount() > 0) {
+                      Element attachment = new Element("icon");
+                      attachment.addAttribute(new Attribute("BUILTIN","attach"));
+                      childElem.appendChild(attachment);
+                      // Append the number of items per priority
+//                    StringBuffer result = new StringBuffer();
+//                    for (Item.PRIORITY priority : Item.PRIORITY.values()) {
+//                        for (Item item : child.items(priority).values()) {
+//                            if (item.getState() == Item.STATE.OPEN) {
+//                                result.append(priority.ordinal());
+//                            }
+//                        }
+//                    }
+//                    Element workPackage = new Element("node");
+//                    workPackage.addAttribute(new Attribute("TEXT",result.toString()));
+//                    childElem.appendChild(workPackage);
+                }
                 parent.appendChild(childElem);
-                createChildProjects(childElem, child);
+                PRIORITY childPriority = createChildProjects(childElem, child);
+                if (maxPriority.ordinal() >  childPriority.ordinal()) maxPriority = childPriority;
             }
         }
+        if (!parentProject.getName().contains("All") &&
+            maxPriority == PRIORITY.URGENT || maxPriority == PRIORITY.TODAY &&
+            parent.getChildElements("icon").size() == 0) {
+            Element urgentIconElem = new Element("icon");
+            urgentIconElem.addAttribute(new Attribute("BUILTIN","messagebox_warning"));
+            parent.appendChild(urgentIconElem);
+        }
+        return maxPriority;
     }
 
     public FreemindProject getRootProject() {
@@ -98,9 +128,9 @@ public class FreemindFile {
     public List<FreemindProject> getChildren(FreemindProject parent) {
         List<FreemindProject> children = new ArrayList<FreemindProject>();
         // rather naive implementation, but good enough for now
-        for (FreemindProject child : parentProjects.keySet()) {
-            if (parentProjects.get(child).getName().equals(parent.getName())) {
-                children.add(child);
+        for (String childName : parentProjects.keySet()) {
+            if (parentProjects.get(childName).getName().equals(parent.getName())) {
+                children.add(projectNames.get(childName));
             }
         }
         return children;
@@ -115,20 +145,23 @@ public class FreemindFile {
             project = new FreemindProject(project);
         }
         if (!contains(project)) {
-            parentProjects.put((FreemindProject)project, root);
+            parentProjects.put(project.getName(), root);
         }
+        projectNames.put(project.getName(), (FreemindProject)project);
     }
     
     public void save(File file) throws Exception {
-        FileWriter writer = new FileWriter(file);
+        FileOutputStream writer = new FileOutputStream(file);
         Element rootElement = new Element("map");
         rootElement.addAttribute(new Attribute("version", "0.7.1"));
         Element allElement = new Element("node");
         allElement.addAttribute(new Attribute("TEXT", root.getName()));
         createChildProjects(allElement, root);
         rootElement.appendChild(allElement);
+        Serializer serializer = new Serializer(writer);
+        serializer.setIndent(2);
         Document document = new Document(rootElement);
-        writer.write(document.toXML());
+        serializer.write(document);
         writer.close();
     }
     
